3630200e0721aa7a1a14a6c3bd828d150e9e0050
Tim Littlemore <tim@tjl2.com>
2022-10-25 22:02:36 +0100

    Add ecto_psql_extras
    So that Live dashboard Ecto stats work

--- mix.exs
+++ mix.exs
@@ -37,6 +37,7 @@ defmodule Pento.MixProject do
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
+      {:ecto_psql_extras, "~> 0.6"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.5"},
