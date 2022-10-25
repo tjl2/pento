ea35e5b176fa2e94918e0e422ea294efcc7e9ccd
Tim Littlemore <tim@tjl2.com>
2022-10-25 22:59:54 +0100

    Finish chapter

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -2,13 +2,16 @@ defmodule PentoWeb.WrongLive do
   use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+    {:ok, assign(socket, score: 0, message: "Make a guess:", time: time())}
   end
 
   def render(assigns) do
     ~H"""
     <h1>Your score: <%= @score %></h1>
-    <h2><%= @message %></h2>
+    <h2>
+      <%= @message %><br />
+      It's <%= @time %>
+    </h2>
     <h2>
       <%= for n <- 1..10 do %>
         <a href="#" phx-click="guess" phx-value-number={n}><%= n %></a>
@@ -17,9 +20,13 @@ defmodule PentoWeb.WrongLive do
     """
   end
 
+  def time() do
+    DateTime.utc_now() |> to_string
+  end
+
   def handle_event("guess", %{"number" => guess} = data, socket) do
     message = "Your guess: #{guess}. Wrong. Guess again."
     score = socket.assigns.score - 1
-    {:noreply, assign(socket, message: message, score: score)}
+    {:noreply, assign(socket, message: message, score: score, time: time())}
   end
 end
