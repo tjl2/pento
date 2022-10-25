4134faebf1b708ae0a5401ef6d3ccbca9893b7bc
Tim Littlemore <tim@tjl2.com>
2022-10-25 22:48:06 +0100

    Start guessing game LiveView

---
+++ lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,25 @@
+defmodule PentoWeb.WrongLive do
+  use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1>Your score: <%= @score %></h1>
+    <h2><%= @message %></h2>
+    <h2>
+      <%= for n <- 1..10 do %>
+        <a href="#" phx-click="guess" phx-value-number={n}><%= n %></a>
+      <% end %>
+    </h2>
+    """
+  end
+
+  def handle_event("guess", %{"number" => guess} = data, socket) do
+    message = "Your guess: #{guess}. Wrong. Guess again."
+    score = socket.assigns.score - 1
+    {:noreply, assign(socket, message: message, score: score)}
+  end
+end
