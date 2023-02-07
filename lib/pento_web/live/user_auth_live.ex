9c72794e4e621b4f0ce2f5c0d990d667bff6db3e
Tim Littlemore <tim@tjl2.com>
2023-02-07 16:03:52 +0000

    Use assign_new
    “We fetch the current user and assign it to the socket with assign_new. This small feature is actually a pretty important one. It means that on the initial mount, we can set the live view’s socket assigns to contain the current user stored in the Plug.Conn assigns. Then, on the second, connected mount, when we no longer have access to the Plug.Conn assigns, we’ll fetch the current user from the database using the token from the session. In this way, we avoid making unnecessary database calls. We only have to execute our “get user” query once, on the second, connected, mount.”

--- lib/pento_web/live/user_auth_live.ex
+++ lib/pento_web/live/user_auth_live.ex
@@ -3,9 +3,9 @@ defmodule PentoWeb.UserAuthLive do
   alias Pento.Accounts
 
   def on_mount(_, _params, %{"user_token" => user_token} = _session, socket) do
-    socket =
-      socket
-      |> assign(:current_user, Accounts.get_user_by_session_token(user_token))
+    socket = assign_new(socket, :current_user, fn ->
+      Accounts.get_user_by_session_token(user_token)
+    end)
 
     if socket.assigns.current_user do
       {:cont, socket}
