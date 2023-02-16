e2ad34f6e402edaab2126d7424c97d8d3968e864
Tim Littlemore <tim@tjl2.com>
2023-02-16 11:42:22 +0000

    Demographics form component

--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -11,11 +11,21 @@ defmodule PentoWeb.SurveyLive do
      |> assign_demographic}
   end
 
+  def handle_info({:created_demographic, demographic}, socket) do
+    {:noreply, handle_demographic_created(socket, demographic)}
+  end
+
   defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
     assign(
       socket,
       :demographic,
       Survey.get_demographic_by_user(current_user)
     )
+  end
+
+  defp handle_demographic_created(socket, demographic) do
+    socket
+    |> put_flash(:info, "Demographic created successfully.")
+    |> assign(:demographic, demographic)
   end
 end
